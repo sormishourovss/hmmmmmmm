7e997df158be1fe7f3365791e7bbb11ad44196f3
𝐊𝐈𝐍𝐆 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 𝐁𝐎𝐓 <164044359+sormishourovss@users.noreply.github.com>
2024-12-05 12:41:33 +0300

    Update Fahim13.js

--- scripts/commands/Fahim13.js
+++ scripts/commands/Fahim13.js
@@ -26,7 +26,7 @@ const media = (
 
 	if (body.indexOf("😭")==0 || body.indexOf("🤧")==0) {
 		var msg = {
-				body: "Md Fahim islam ",
+				body: "𝐊𝐢𝐧𝐠_𝐒𝐡𝐨𝐮𝐫𝐨𝐯 ",
 				attachment: media
 			}
 			api.sendMessage( msg, threadID, messageID);
