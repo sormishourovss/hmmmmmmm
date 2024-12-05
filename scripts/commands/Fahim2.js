956300494fcafbd184245cf1355d1024fb8976ef
𝐊𝐈𝐍𝐆 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 𝐁𝐎𝐓 <164044359+sormishourovss@users.noreply.github.com>
2024-12-05 13:09:34 +0300

    Update Fahim2.js

--- scripts/commands/Fahim2.js
+++ scripts/commands/Fahim2.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("☞♥♪✆😘😍🥵🤯 Fahim islam ☞♥♪✆😘😍🥵🤯")==0 || (event.body.indexOf("Fahim")==0 || (event.body.indexOf("fahim")==0 || (event.body.indexOf("ফাহিম")==0)))) {
+	if (event.body.indexOf("king shourov")==0 || (event.body.indexOf("Shourov")==0 || (event.body.indexOf("shourov")==0 || (event.body.indexOf("সৌরভ")==0)))) {
 		var msg = {
 				body: " কিরে এতো ডাকিস কেন আমার বস বিজি আসে দেখস না গা্ৃ্ঁধা্ঁ 🤬:))"
     }
