0b3cc259aec70c2b0b5e964cf5496f3160f8e4ce
𝐊𝐈𝐍𝐆 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 𝐁𝐎𝐓 <164044359+sormishourovss@users.noreply.github.com>
2024-12-05 18:25:29 +0300

    Update 000.js

--- scripts/commands/000.js
+++ scripts/commands/000.js
@@ -28,20 +28,20 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       : tasfiya mitu
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : tasfiya jannat mitu
+𝐍𝐚𝐦𝐞       : Israt Jahan
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : Israt Jahan
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: rangpur, Dhaka, Bangladesh 
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Dhaka, Bangladesh 
 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Bola jabe na
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 
-𝐀𝐠𝐞           :18+
+𝐀𝐠𝐞           :1
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : সৌ্ঁর্ঁভ্ঁ এর বউ
 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : shourovislam5430@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801709281334
+𝐆𝐦𝐚𝐢𝐥       : dimu na
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: i love u
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : ওই সব বাল চালাই না😡
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 :https://www.facebook.com/broken.shourov.ss?`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 :https://www.facebook.com/israt.jahan.900643?p`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://i.postimg.cc/BbxbBzGX/3943d15d-176e-4f98-b47f-e6b901b4de94.jpg`)).pipe(
+      return request(encodeURI(`https://i.postimg.cc/XJDjgQDG/462577474-912390647693822-8692499326659885471-n-2.jpg`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
